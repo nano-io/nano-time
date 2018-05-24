7188089e10548731412982297f66fbd48b8fcba5
Mark <mark@knoxroad.net>
2018-05-24 17:38:12 +0100

    Add native loader with ability to load lib from jar

--- src/main/java/io/nano/time/NanoTime.java
+++ src/main/java/io/nano/time/NanoTime.java
@@ -2,13 +2,24 @@ package io.nano.time;
 
 public class NanoTime {
 
+    private static final String LIB_NAME = "nanotime";
+
+    private static boolean libraryLoaded = false;
+
     static {
-        System.loadLibrary("nanotime");
-//        System.load(".../target/lib/libnanotime.so");
+        NativeLoader.loadNativeLibrary(LIB_NAME);
     }
 
+    /**
+     * Returns the current wall clock time to microsecond precision
+     * @return current tim in microseconds
+     */
     public static native long currentTimeMicros();
 
+    /**
+     * Returns the current wall clock time to nanosecond precision
+     * @return current tim in nanoseconds
+     */
     public static native long currentTimeNanos();
 
 }
